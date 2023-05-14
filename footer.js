3f91168325b4616ad6ddd4d6079997b565d6d8f0
Vasil Melnik <vasiliymelnik64@gmail.com>
2023-05-14 11:18:37 +0200

    12312312

--- footer.js
+++ footer.js
@@ -1,1 +1,5 @@
-// test
+// go to branch master
+// git pull origin master
+// git checkout -b melnik/header-logo - create your branch
+// git add . or "+" in vscode
+// git commit -m "added header" or in input in vscode
