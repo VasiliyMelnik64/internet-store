eb6780ba04185f891e6c4d01bc0f19bc316e1b88
Vasil Melnik <vasiliymelnik64@gmail.com>
2023-05-14 11:30:20 +0200

    Revert "sfsdf"
    This reverts commit 315c4c310003aad51eff737c4ae4a56756197585.

--- footer.js
+++ footer.js
@@ -1,9 +1,9 @@
 // go to branch master
-// run command git pull origin master
-// run command git checkout -b melnik/header-logo - create your branch
-// run command git add . or "+" in vscode
-// run command git commit -m "added header" or in input in vscode
-// run command git push origin melnik/header-logo or "publish" in vscode
+// git pull origin master
+// git checkout -b melnik/header-logo - create your branch
+// git add . or "+" in vscode
+// git commit -m "added header" or in input in vscode
+// git push origin melnik/header-logo or "publish" in vscode
 // go to github -> branches -> choose your branch -> "Create new pull request"
 // write name for pull request
 // "create pull request"
